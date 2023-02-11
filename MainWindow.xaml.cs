23e671e7e07c77b5061c56b86166939479d2ebc8
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-02-10 16:56:21 -0800

    Starting
    first

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,22 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+//What is a method?
+//A block of code that is given a name and runs when it is called
+
+//What is the purpose of a method?
+//Reuse a block of code over and over to be more effiecient
+
+//What is a class method?
+
+//Review: What’s the name for any field, constructor, property or method that is part of a class?
+//Review: What are the 4 parts of declaring a method?
+//What keyword allows you to access members related to the specific instance?
+//A method build inside a class has access to what, even it’s access modifier is set to private?
+//What does “override .ToString()” allow us to do?
+//You use the *____ operator* do you use to access members inside of an instanced class?
+
 namespace Lecture6Notes
 {
     /// <summary>
---
+++ Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Notes
+{
+    public class Student
+    {
+        string _firstName;
+        string _lastName;
+        double _csiGrade;
+        double _genEdGrade;
+
+        public Student(string firstName, string lastName, double csiGrade, double genEdGrade)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _csiGrade = csiGrade;
+            _genEdGrade = genEdGrade;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public double CsiGrade { get => _csiGrade; set => _csiGrade = value; }
+        public double GenEdGrade { get => _genEdGrade; set => _genEdGrade = value; }
+    }
+}
